76e1b0329b2e445238dbeb2fef97f8358a27d5ba
M-J-Bradley <mbradley1@hotmail.co.nz>
2023-07-02 15:24:48 +0100

    updated helloWorld.py to print Git is Awesome

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+# Compulsory Task 1
+# Hello World
+# This code is designed to obtain a name and age from the user, finally printing the string "Hello World"
+
+print("Git is Awesome!")
+
+# end of code
+# Matthew Bradley  02 July 2023
